a62202bf5b9fae47be6b7b5323b8e7b8ab9152bf
Nerijus <dnerijus72@gmail.com>
2022-09-22 09:08:11 +0300

    add search and minimap to nvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -34,6 +34,11 @@ lvim.keys.normal_mode["<C-j>"] = ":ToggleTerm<cr>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
+lvim.keys.normal_mode["<leader>S"] = "<cmd>lua require('spectre').open()<CR>"
+lvim.keys.normal_mode["<leader>sw"] = "<cmd>lua require('spectre').open_visual({select_word=true})<CR>"
+lvim.keys.normal_mode["<leader>mm"] = ":MinimapToggle<cr>"
+lvim.keys.normal_mode["<leader>mr"] = ":MinimapRefresh<cr>"
+
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
@@ -84,17 +89,14 @@ vim.g.NERDTreeWinSize = 60
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
-  "c",
   "javascript",
   "json",
   "lua",
-  "python",
   "typescript",
   "tsx",
   "css",
-  "rust",
-  "java",
   "yaml",
+  "go",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -191,6 +193,8 @@ linters.setup {
 lvim.plugins = {
   { "folke/tokyonight.nvim" },
   { "nvim-treesitter/nvim-treesitter-context"},
+  { "windwp/nvim-spectre" },
+  { "wfxr/minimap.vim" },
   --     {
   --       "folke/trouble.nvim",
   --       cmd = "TroubleToggle",
